aae386507fc45e4b5fbeb0523b2b993e14946186
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-16 10:56:55 -0400

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
-import express from "express"
+import express,{Request,Response} from "express"
 const app = express()
 
 
-app.get('/hello',(req,res)=>{
+app.get('/hello',(req:Request,res:Response)=>{
     res.send("hello world")
 })
+
+ 
+
 app.listen(5000);
+ 
