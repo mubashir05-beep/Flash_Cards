e37dbb6d52b6eb7404aec2103bd7ffc7d0a188e7
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-17 13:27:56 -0400

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState('');
+
+  const handleInputChange = (e) => {
+    setData(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:9000/decks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: data, // Assuming your server expects a 'title' field
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+  
+      // Handle the form submission if needed
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleInputChange} value={data} />
+        <input type='submit' />
+      </form>
+      <div>{data}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,19 +4,20 @@ import DeckModel from "./models/Deck";
 import { config } from "dotenv";
 config();
 const app = express();
-
+import cors from "cors"
 const PORT = 9000;
 
 app.use(express.json());
+app.use(cors())
 
-app.get("/hello", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
 app.post("/decks", async (req: Request, res: Response) => {
   console.log(req.body);
   const newDeck = new DeckModel({
-    title: req.body.title,
+    title: req.body.title
   });
   const createdDeck = await newDeck.save();
   res.json(createdDeck);
