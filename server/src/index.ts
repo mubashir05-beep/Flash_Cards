6f34ffb0e264cff6d13ba14d65611dd5fe03eb83
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-17 07:56:35 -0400

    added env

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,31 +1,28 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import DeckModel from "./models/Deck";
-
+import { config } from "dotenv";
+config();
 const app = express();
 
 const PORT = 9000;
 
 app.use(express.json());
 
-app.get("/hello",  (req: Request, res: Response) => {
+app.get("/hello", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
-app.post('/decks', async  (req: Request, res: Response) => {
-  console.log(req.body)
-    const newDeck = new DeckModel({
-        title: req.body.title,
-    });
-    const createdDeck = await newDeck.save();
-    res.json(createdDeck);
+app.post("/decks", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const newDeck = new DeckModel({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
 });
 
-mongoose
-  .connect(
-    "mongodb+srv://flashcard:mubmud@cluster0.gvjjzlx.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => {
-    console.log(`Listening on port ${PORT}`);
-    app.listen(PORT);
-  });
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`Listening on port ${PORT}`);
+  app.listen(PORT);
+});
