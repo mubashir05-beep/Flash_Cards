a859181b39fb0b806d04a24a9adee1cc38a2764c
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-20 08:36:01 -0400

    update

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL ?? "https://flash-cards-three.vercel.app/";
+export const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:9000/";
--- server/src/index.ts
+++ server/src/index.ts
@@ -49,4 +49,14 @@ mongoose.connect(process.env.MONGO_URL!).then(() => {
   }
 });
 
-module.exports=app;
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
