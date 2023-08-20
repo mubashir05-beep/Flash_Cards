ce94d733de8e74807b3f4e001cca044dbc15a380
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-20 05:51:25 -0400

    Update config.ts

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL ?? "https://flash-cards-backend-pi.vercel.app";
+export const API_URL = import.meta.env.VITE_API_URL ?? "https://flash-cards-three.vercel.app/";
