068642b300d444f38a0974980b896ade20b3b209
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-21 06:07:58 -0400

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,16 +15,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: [
-      "https://flash-cards-app-tau.vercel.app/",
-      "https://flash-cards-api-eight.vercel.app/",
-    ], // This should be a string or an array of allowed origins
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Specify the allowed HTTP methods
-    credentials: true, // Allow credentials (cookies, HTTP authentication)
-  })
-);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("hello");
@@ -42,6 +33,12 @@ app.get("/decks/:decksid", fetchDeckCardController);
 
 app.delete("/decks/:decksid/cards/:index", deleteDeckCard);
 
+app.get("/api/hello/", (req, res) => {
+  res.json({
+      message: "Hello World"
+  });
+});
+
 mongoose.connect(process.env.MONGO_URL!).then(() => {
   console.log(`Listening on port ${process.env.PORT}`);
   if (process.env.PORT) {
@@ -49,14 +46,3 @@ mongoose.connect(process.env.MONGO_URL!).then(() => {
   }
 });
 
-if (process.env.NODE_ENV === "production") {
-  const path = require("path");
-  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
-  app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
-          if(err) {
-              res.status(500).send(err)
-          }
-      });
-  })
-}
