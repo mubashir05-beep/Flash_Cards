c4fe2d2966fc177d199f71595f8e028cdf2955e6
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-22 07:48:32 -0400

    update

--- server/src/controllers/createDeckController.ts
+++ server/src/controllers/createDeckController.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
-import { ParamsDictionary } from "express-serve-static-core"; // Import ParamsDictionary type
 import DeckModel from "../models/Deck";
 
-export const createDeckController = async (req: Request<ParamsDictionary, any, any, { title: string }>, res: Response<any>) => {
-    const newDeck = new DeckModel({
-      title: req.body.title
-    });
-    const createdDeck = await newDeck.save();
-    res.json(createdDeck);
-}
+export const createDeckController = async (req: Request<any, any, { title: string }>, res: Response<any>) => {
+    try {
+        const newDeck = new DeckModel({
+            title: req.body.title
+        });
+        const createdDeck = await newDeck.save();
+        res.json(createdDeck);
+    } catch (error) {
+        console.error("Error creating deck:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
