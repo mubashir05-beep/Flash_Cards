b0aa180304c612bbbcdc0777ca64618d2c5d05c6
Muhammad Mubashir Munir Khan <100374421+mubashir05-beep@users.noreply.github.com>
2023-08-22 08:00:57 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+// import express from "express";
 // import mongoose from "mongoose";
 // import { createDeckController } from "./src/controllers/createDeckController";
 // import { deleteDeckController } from "./src/controllers/deleteDeckController";
@@ -50,20 +50,65 @@ import express from "express";
 
 
 
-const app = express()
-const PORT = 4000
+// const app = express()
+// const PORT = 4000
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `)
+// })
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hey this is my API running 🥳')
+// })
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ')
-})
+// app.get('/about', (req, res) => {
+//   res.send('This is my about route..... ')
+// })
 
-// Export the Express API
+// // Export the Express API
+
+
+import express from "express";
+import mongoose from "mongoose";
+import { createDeckController } from "./src/controllers/createDeckController";
+import { deleteDeckController } from "./src/controllers/deleteDeckController";
+import { fetchDeckController } from "./src/controllers/fetchDeckController";
+import { createDeckCardController } from "./src/controllers/createDeckCardController";
+import { fetchDeckCardController } from "./src/controllers/fetchDeckCardController";
+import { deleteDeckCard } from "./src/controllers/deleteDeckCard";
+import cors from "cors";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+// Define your routes here
+// app.delete("/decks/:decksid", deleteDeckController);
+// app.post("/decks", createDeckController);
+// app.get("/decks", fetchDeckController);
+// app.post("/decks/:decksid/cards", createDeckCardController);
+// app.get("/decks/:decksid", fetchDeckCardController);
+// app.delete("/decks/:decksid/cards/:index", deleteDeckCard);
+
+app.get("/api/hello/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+mongoose.connect(process.env.MONGO_URL!).then(() => {
+  console.log(`Listening on port ${process.env.PORT}`);
+  if (process.env.PORT) {
+    app.listen(process.env.PORT);
+  }
+});
 module.exports = app
